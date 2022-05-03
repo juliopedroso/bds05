f8feebd79b533edee615c3af32ddf96857607a6d
Julio Cesar Pedroso <juliocp@weg.net>
2022-05-03 12:21:58 -0300

    Review created

---
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -0,0 +1,62 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private Movie movie;
+    private User user;
+
+    public Review() {
+    }
+
+    public Review(Long id, String text, Movie movie, User user) {
+        this.id = id;
+        this.text = text;
+        this.movie = movie;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
