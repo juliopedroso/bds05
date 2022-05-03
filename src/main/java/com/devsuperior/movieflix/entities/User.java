2e779366dee657f39e04e64c51c96232edf4311c
Julio Cesar Pedroso <juliocp@weg.net>
2022-05-03 16:03:24 -0300

    User entity created

---
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -0,0 +1,75 @@
+package com.devsuperior.movieflix.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<Review> reviews = new ArrayList<>(); 
+    private List<Role> roles = new ArrayList<>(); 
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Review> getReviews() {
+        return this.reviews;
+    }
+
+    public List<Role> getRoles() {
+        return this.roles;
+    }
+
+}
