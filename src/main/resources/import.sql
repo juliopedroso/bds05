0a4fabb68d8a9c344a5444831459425bce65d0c1
Julio Cesar Pedroso <juliocp@weg.net>
2022-05-06 16:34:08 -0300

    Database seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,7 +11,14 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_genre (name) VALUES ('suspense');
+INSERT INTO tb_genre (name) VALUES ('ação');
+INSERT INTO tb_genre (name) VALUES ('desenho');
 
-INSERT INTO tb_genre (name) VALUES ("suspense");
-INSERT INTO tb_genre (name) VALUES ("ação");
-INSERT INTO tb_genre (name) VALUES ("desenho");
+INSERT INTO tb_movie (title, sub_title, year, img_url,synopsis,genre_id) VALUES ('007', 'Moscou contra 007',1963,'http://server/img1.jpg','sinopse filme 1',2);
+INSERT INTO tb_movie (title, sub_title, year, img_url,synopsis,genre_id) VALUES ('008', 'Moscou contra 008',1964,'http://server/img2.jpg','sinopse filme 2',3);
+INSERT INTO tb_movie (title, sub_title, year, img_url,synopsis,genre_id) VALUES ('009', 'Moscou contra 009',1965,'http://server/img3.jpg','sinopse filme 3',1);
+
+INSERT INTO tb_review (text, movie_id, user_id) VALUES ('Filme muito bom', 1, 1);
+INSERT INTO tb_review (text, movie_id, user_id) VALUES ('Filme bem divertido', 2, 2);
+INSERT INTO tb_review (text, movie_id, user_id) VALUES ('Filme muito trist', 3, 1);
